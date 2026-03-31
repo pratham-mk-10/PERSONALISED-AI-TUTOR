dd107585c048f64f82207dc351ca3e68fa1733cc
Rachandevadiga <rachandevadiga4@gmail.com>
2026-03-31 09:24:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import react from "react";
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import QuizPage from "./components/quiz/QuizPage";
 import LawsOfReflectionAnimation from "./svg-engine/reflection/animations/LawsOfReflectionAnimation";
 import AngleSlider from "./svg-engine/reflection/interactive/AngleSlider";
